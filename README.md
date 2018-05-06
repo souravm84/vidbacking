53367375055bb652a6684c7a6746543e102eaebc
Sourav Mukhopadhyay <souravmukhopadhyay@hotmail.com>
2018-05-06 23:36:03 +0530

    Version 2.0.0 pushed with Youtube video support.

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-# Video Background Plugin
-##### vidbacking is an attempt to make HTML5 video-background easy.
+# Video Background Plugin with Support for Youtube Video
+##### vidbacking is an attempt to make HTML5 video-background easy. Support video files or Youtube video
 [![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://raw.githubusercontent.com/souravm84/vidbacking/master/LICENSE.md)
 
-This is a small jQuery plugin to simplify the process of showing HTML5 video as background. You can use this for showing a video on **whole page background** or to show the video as the background of a `<div>`, `<section>`, like elements.
+This is a small jQuery plugin to simplify the process of showing HTML5 video as background. You can use this for showing a video file or Youtube video on **whole page background** or to show the video as the background of a `<div>`, `<section>`, like elements.
 >  ***Features:***
 > * Cross Browser Video Background Plugin.
+> * Support mp4, webm video files.
+> * Support Youtube video.
 > * Very small file size (4kb total).
 > * Easy to use.
 > * Can be used for both page background or for an element background.
@@ -14,13 +16,17 @@ This is a small jQuery plugin to simplify the process of showing HTML5 video as 
 > * Device friendly, responsive.
 > * Tested on all modern browsers.
 
- 
+
 ### Demo
 Check few demos in action.
 
-> [Full page background video example](https://souravm84.github.io/vidbacking/demo1/) 
-> 
-> [Video as a `<div>` background](https://souravm84.github.io/vidbacking/demo2/)
+> [Full page background example with mp4, webm video files](https://souravm84.github.io/vidbacking/demo1/)
+>
+> [Video as a `<div>` background  with mp4, webm video files](https://souravm84.github.io/vidbacking/demo2/)
+>
+>[Full page background example with Youtube video](https://souravm84.github.io/vidbacking/demo3/)
+>
+> [Video as a `<div>` background  with Youtube video](https://souravm84.github.io/vidbacking/demo2/)
 
 ### Dependency
 The **vidbacking** depends on jQuery 2.x or higher. No other files required for it to work properly. Also, it requires HTML5 video tag support in the browser(This is a common feature in all major browsers).
@@ -31,7 +37,7 @@ You can clone/download it from github.com directly. This plugin is also availabl
 
 ### How to use
 You can use this video-background plugin in two different way. Either for video as page background, or for the background of a `<div>`, `<section>`, or some other block level elements.
-#### For full page background:
+#### For full page background with mp4, or webm video file:
 
 You need to add the HTML5 video tag next to  your HTML body tag. Don't forget to add the ***class="vidbacking"*** in your video tag.
 
@@ -57,7 +63,7 @@ Then import the script after importing jQuery file, and write a little script as
 </script>
 ```
 
-#### For `<div>` or other block level elements' background:
+#### For `<div>` or other block level elements' background with mp4, or webm video file:
 
 You need to add the HTML5 video tag under the `<div>` or block level element for which you want to set a video-background. Don't forget to add the ***class="vidbacking"*** in your video tag.
 ```html
@@ -88,12 +94,68 @@ Then import the script after importing jQuery file, and write a little script as
 ```
 > Note: don't forget to import jQuery before adding the above script.
 
+#### For full page background with Youtube Video:
+
+You need to add the `<iframe>` tag next to  your HTML body tag. You can get that iframe code under Youtube video's share -> Embed section. Copy the entire iframe tag provided there. Paste it in your HTML and add the ***class="vidbacking"*** in that iframe tag.
+
+```html
+<iframe width="560" height="315" src="https://www.youtube.com/embed/2NYwewKYHB4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen  class="vidbacking"></iframe>
+```
+Link the css file jquery.vidbacking.min.css provided under the `dist` folder under your `<head />` tag.
+```html
+<link rel="stylesheet" href="dist/jquery.vidbacking.css" type="text/css">
+```
+Then import the script after importing jQuery file, and write a little script as described in following section.
+
+```html
+<script type="text/javascript" src="dist/jquery.vidbacking.js"></script>
+<script type="text/javascript">
+	$(function(){
+    		$('body').vidbacking();
+	});
+</script>
+```
+#### For `<div>` or other block level elements' background with Youtube video:
+
+You need to add the `<iframe>` tag under the `<div>` or block level element for which you want to set a video-background. You can get that iframe code under Youtube video's share -> Embed section. Copy the entire iframe tag provided there. Paste it in your HTML and add the ***class="vidbacking"*** in that iframe tag.
+```html
+<div id="video-back">
+	<iframe width="560" height="315" src="https://www.youtube.com/embed/2NYwewKYHB4" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen  class="vidbacking"></iframe>
+    <!-- html content of the div -->
+    <h1>Vidbacking Demo of &lt;div&gt; Background</h1>
+</div>
+```
+
+Link the css file jquery.vidbacking.min.css provided under the `dist` folder under your `<head />` tag.
+```html
+<link rel="stylesheet" href="dist/jquery.vidbacking.css" type="text/css">
+```
+
+Then import the script after importing jQuery file, and write a little script as described in following section.
+
+```html
+<script type="text/javascript" src="dist/jquery.vidbacking.js"></script>
+<script type="text/javascript">
+	$(function(){
+    		$('#video-back').vidbacking({
+	    		'masked': true
+    		});
+	});
+</script>
+```
+> Note: don't forget to import jQuery before adding the above script.
+
+
 ### Plugin parameters
 This plugin support following parameters. You can change the output of vidbacking by setting these parameters.
-> ***masked:*** This parameter can have two possible values `true` or `false`. If set to `true` will masked the video with a png image overlay.
-> 
-> ***mask-opacity:*** If `masked` is `true` then by setting this parameter you can change the opacity of the mask image. Possible value ranged between `1` to `0`, for example `mask-opacity: 0.25`.
-> 
-> ***video-opacity:*** You can set the video opacity using this parameter. Possible value ranged between `1` to `0`, for example `video-opacity: 0.5`.
+> ***masked:*** This parameter can have two possible values `true` or `false`. If set to `true` will masked the video with a png image overlay. Default Value `false`.
+>
+> ***mask-opacity:*** If `masked` is `true` then by setting this parameter you can change the opacity of the mask image. Possible value ranged between `1` to `0`, for example `mask-opacity: 0.25`. Default value `1`
+>
+> ***video-opacity:*** You can set the video opacity using this parameter. Possible value ranged between `1` to `0`, for example `video-opacity: 0.5`. This option doesn't work on Youtube videos. Default value `1`
+>
+>***youtube-mute-video:*** Setting this parameter to `true` makes the Youtube video muted. Default value is `true`.
+>
+>***youtube-loop:*** Set this parameter "true" to loop the YouTube video. Possible values are `true` and `false`. Default value is `true`.
 
 I have plans for upgrading this plugin with more functionalities. If you found any issues please report in this repo, I will try to fix them in next release.
--- src/jquery.vidbacking.js
+++ src/jquery.vidbacking.js
@@ -2,7 +2,7 @@
 * vidbacking
 * jQuery Background video plugin for jQuery
 * ---
-* Copyright 2017, Sourav Mukhopadhyay (https://www.upwork.com/o/profiles/users/_~010a5ed52d2f45f4db/)
+* Copyright 2018, Sourav Mukhopadhyay (https://www.upwork.com/o/profiles/users/_~010a5ed52d2f45f4db/)
 * Released under the MIT Licenses.
 * Description: Allow embedding HTML5 video as page background, and element background.
 * Can be used on multiple elements background on same page.
@@ -10,67 +10,238 @@
 (function($){
 	$.fn.vidbacking = function(options){
 		var settings = $.extend({
-			'video-opacity': '1',
-			'masked': false,
-			'mask-opacity': '1'
+			'video-opacity': '1', //This parameter defines Video Opacity(It can't change YouTube video opacity).
+			'masked': false,      //If this parameter is true then a dotted image will overlay the video.
+			'mask-opacity': '1',  // It defines the mask image opacity. Only effective if "masked" is "true".
+			'youtube-mute-video': true, //If you want the YouTube video to be muted.
+			'youtube-loop': true        //Set this parameter "true" to loop the YouTube video.
 		},options);
-		
+
 		return this.each(function(){
 			var targetobj = $(this),
 				targettag = targetobj.prop('tagName'),
 				targetid = targetobj.prop('id');
-			
+
+			//This section execute when the Target Object is <BODY>.
 			if(targettag == 'BODY'){
-				obj = $('.vidbacking');
-				poster = obj.attr('poster');
-				obj.css('background-size','100% 100% !important');
-				obj.css('background-image','url('+poster+')');
-				obj.addClass('vidbacking-active-body-back');
-				obj.css('opacity',settings["video-opacity"]);
+				obj = $('.vidbacking');  //The video object
+				objtag = obj.prop('tagName'); //The tag of the video(<IFRAME> for YouTube or <video>).
+				//This section execute when YouTube iframe tag found as background video.
+				if(objtag == 'IFRAME'){
+					var youtube_source = obj.attr('src') + '?autoplay=1&rel=0&controls=0&showinfo=0',
+						video_url_parts = obj.attr('src').split('/'),
+						video_id = video_url_parts[video_url_parts.length - 1];
+					//Check and set YouTube video mute if "youtube-mute-video" parameter set to "true".
+					if(settings['youtube-mute-video'] == true){
+						youtube_source += '&mute=1';
+					}
+					//Make the YouTube video loop if "youtube-loop" parameter set to "true".
+					if(settings['youtube-loop'] == true){
+						youtube_source += '&loop=1&playlist='+video_id;
+					}
+					obj.attr('src',youtube_source);
+					obj.addClass('vidbacking-active-body-back-youtube');
+					var winh = $(window).height(), //Browser Window height
+						winw = $(window).width(),  //Browser Window width
+						vidh = obj.height(),       //video object height
+						vidw = obj.width(),        //video object window
+						wre = winw / winh,         //ratio of window width vs window height
+						hre = winh / winw;         //ratio of window height vs window width
+
+					//This section will execute if window width is greater than window height.
+					if(winw > winh){
+						if(wre < 1.77){
+							expected_width = winh * (16/9);
+							obj.css('width',expected_width+'px');
+							obj.css('height','100%');
+						}else{
+							expected_height = winw / (16/9);
+							obj.css('width','100%');
+							obj.css('height',expected_height+'px');
+						}
+					//This section will execute if window height is greater than window width.
+					}else{
+						if(hre < 0.56){
+							expected_height = winw / (16/9);
+							obj.css('width','100%');
+							obj.css('height',expected_height+'px');
+						}else{
+							expected_width = winh * (16/9);
+							obj.css('width',expected_width+'px');
+							obj.css('height','100%');
+						}
+					}
+
+				//This section execute when a HTML5 <video> is set as background video.
+				}else{
+					poster = obj.attr('poster');
+					obj.css('background-size','100% 100% !important');
+					obj.css('background-image','url('+poster+')'); //Setting the poster image as background.
+					obj.addClass('vidbacking-active-body-back');
+					obj.css('opacity',settings["video-opacity"]);  //Setting the video opacity.
+				}
 				obj.removeClass('vidbacking');
-				
+
+				//Check and set the image mask over video according to the value of "masked" parameter.
 				if(settings.masked == true){
 					obj.after('<div class="vidmask-body-back">&nbsp;</div>');
 					$('.vidmask-body-back').css('opacity',settings["mask-opacity"]);
 				}
+				//On Browser Window reset this code will execute.
 				$(window).resize(function(){
-					var winh = $(window).height(),
-						winw = $(window).width(),
-						vidh = obj.height(),
-						vidw = obj.width();
-
-					if(vidh < winh){
-						obj.css('height',winh);
-					}
-					if(vidw < winw){
-						obj.css('width',winw);
+					var winh = $(window).height(),  //Browser window height.
+						winw = $(window).width(),   //Browser window width.
+						vidh = obj.height(),        //Video object height.
+						vidw = obj.width(),         //Video object width.
+						wre = winw / winh,          //ratio of window width vs window height.
+						hre = winh / winw;          //ratio of window height vs window width.
+					//This section execute when YouTube iframe tag found as background video.
+					if(objtag == 'IFRAME'){
+						//This section will execute if window width is greater than window height.
+						if(winw > winh){
+							if(wre < 1.77){
+								expected_width = winh * (16/9);
+								obj.css('width',expected_width+'px');
+								obj.css('height','100%');
+							}else{
+								expected_height = winw / (16/9);
+								obj.css('width','100%');
+								obj.css('height',expected_height+'px');
+							}
+						//This section will execute if window height is greater than window width.
+						}else{
+							if(hre < 0.56){
+								expected_height = winw / (16/9);
+								obj.css('width','100%');
+								obj.css('height',expected_height+'px');
+							}else{
+								expected_width = winh * (16/9);
+								obj.css('width',expected_width+'px');
+								obj.css('height','100%');
+							}
+						}
+					//This section execute when a HTML5 <video> is set as background video.
+					}else{
+						//When video height is less than Browser window height.
+						if(vidh < winh){
+							obj.css('height',winh);
+						}
+						//When video width is less than Browser window width.
+						if(vidw < winw){
+							obj.css('width',winw);
+						}
 					}
 				});
+			//This section execute when the Target Object is any HML block element
+			//Such as <div>,<section>,etc. Othen than <BODY>.
 			}else{
-				obj = $(this).find('.vidbacking');
-				poster = obj.attr('poster');
-				targetobj.css('position','relative');
-				targetobj.css('overflow','hidden');
-				obj.css('background-image','url('+poster+')');
-				obj.addClass('vidbacking-active-block-back');
-				obj.css('opacity',settings["video-opacity"]);
+				obj = $(this).find('.vidbacking'); //The video object
+				objtag = obj.prop('tagName');   //The tag of the video(<IFRAME> for YouTube or <video>).
+				//This section execute when YouTube iframe tag found as background video.
+				if(objtag == 'IFRAME'){
+					var youtube_source = obj.attr('src') + '?autoplay=1&rel=0&controls=0&showinfo=0',
+						video_url_parts = obj.attr('src').split('/'),
+						video_id = video_url_parts[video_url_parts.length - 1];
+					//Check and set YouTube video mute if "youtube-mute-video" parameter set to "true".
+					if(settings['youtube-mute-video'] == true){
+						youtube_source += '&mute=1';
+					}
+					//Make the YouTube video loop if "youtube-loop" parameter set to "true".
+					if(settings['youtube-loop'] == true){
+						youtube_source += '&loop=1&playlist='+video_id;
+					}
+					obj.attr('src',youtube_source);
+					obj.addClass('vidbacking-active-block-back-youtube');
+					targetobj.css('position','relative');
+					targetobj.css('overflow','hidden');
+					var winh = targetobj.outerHeight(), //Get the target element height.
+						winw = targetobj.width(),       //Get the target element width.
+						vidh = obj.height(),            //Video object height.
+						vidw = obj.width(),             //Video object width.
+						wre = winw / winh,              //ratio of target element width vs height.
+						hre = winh / winw;              //ratio of target element height vs window width.
+					//This section will execute if target element width is greater than height.
+					if(winw > winh){
+						if(wre < 1.77){
+							expected_width = winh * (16/9); //"16/9" is the default aspect ration of YouTube video.
+							obj.css('width',expected_width+'px');
+							obj.css('height','100%');
+						}else{
+							expected_height = winw / (16/9);//"16/9" is the default aspect ration of YouTube video.
+							obj.css('width','100%');
+							obj.css('height',expected_height+'px');
+
+						}
+					//This section will execute if target element height is greater than width.
+					}else{
+						if(hre < 0.56){
+							expected_height = winw / (16/9);  //"16/9" is the default aspect ration of YouTube video.
+							obj.css('width','100%');
+							obj.css('height',expected_height+'px');
+						}else{
+							expected_width = winh * (16/9);   //"16/9" is the default aspect ration of YouTube video.
+							obj.css('width',expected_width+'px');
+							obj.css('height','100%');
+						}
+					}
+				//This section execute when a HTML5 <video> is set as background video.
+				}else{
+					poster = obj.attr('poster');
+					targetobj.css('position','relative');
+					targetobj.css('overflow','hidden');
+					obj.css('background-image','url('+poster+')');  //Setting the poster image as background.
+					obj.addClass('vidbacking-active-block-back');
+					obj.css('opacity',settings["video-opacity"]);   //Setting the video opacity.
+				}
 				obj.removeClass('vidbacking');
+
+				//Check and set the image mask over video according to the value of "masked" parameter.
 				if(settings.masked == true){
 					targetobj.append('<div class="vidmask-block-back">&nbsp;</div>');
 					targetobj.find('.vidmask-block-back').css('opacity',settings["mask-opacity"]);
 				}
 				$(window).resize(function(){
-					var winh = targetobj.height(),
-						winw = targetobj.width(),
-						vidh = obj.height(),
-						vidw = obj.width();
+					var winh = targetobj.outerHeight(),  //Get the target element height.
+						winw = targetobj.width(),        //Get the target element width.
+						vidh = obj.height(),             //Video object height.
+						vidw = obj.width(),              //Video object width.
+						wre = winw / winh,               //ratio of target element width vs height.
+						hre = winh / winw;               //ratio of target element height vs window width.
+					//This section execute when YouTube iframe tag found as background video.
+					if(objtag == 'IFRAME'){
+						//This section will execute if target element width is greater than height.
+						if(winw > winh){
+							if(wre < 1.77){
+								expected_width = winh * (16/9);//"16/9" is the default aspect ration of YouTube video.
+								obj.css('width',expected_width+'px');
+								obj.css('height','100%');
+							}else{
+								expected_height = winw / (16/9);//"16/9" is the default aspect ration of YouTube video.
+								obj.css('width','100%');
+								obj.css('height',expected_height+'px');
+							}
+						//This section will execute if target element height is greater than width.
+						}else{
+							if(hre < 0.56){
+								expected_height = winw / (16/9);//"16/9" is the default aspect ration of YouTube video.
+								obj.css('width','100%');
+								obj.css('height',expected_height+'px');
+							}else{
+								expected_width = winh * (16/9);//"16/9" is the default aspect ration of YouTube video.
+								obj.css('width',expected_width+'px');
+								obj.css('height','100%');
+							}
+						}
+					//This section execute when a HTML5 <video> is set as background video.
+					}else{
+						if(vidh < winh){
+							obj.css('height',winh);
+						}
+						if(vidw < winw){
+							obj.css('width',winw);
+						}
+					}
 
-					if(vidh < winh){
-						obj.css('height',winh);
-					}
-					if(vidw < winw){
-						obj.css('width',winw);
-					}
 				});
 				//checking if out of focus on scroll
 				$(window).scroll(function(){
@@ -80,6 +251,7 @@
 						target_element_position = targetobj.offset(),
 						target_element_bottom = targetobj.outerHeight() + target_element_position.top,
 						window_bottom = winheight + top;
+					//If out of view port then pause HTML5 video. No effect on YouTube video.
 					if(target_element_bottom < top || target_element_position.top > window_bottom){
 						if(!obj[0].paused){
 							try{
@@ -88,6 +260,7 @@
 								console.error(e);
 							}
 						}
+					//If in view port then play HTML5 video. No effect on YouTube video.
 					}else{
 						if(obj[0].paused){
 							try{
@@ -98,9 +271,9 @@
 						}
 					}
 				});
-				
+
 			}
-			
+
 		});
 	}
-})(jQuery);
+})(jQuery);//End of plugin code.
